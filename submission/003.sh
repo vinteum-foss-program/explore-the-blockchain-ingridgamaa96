887283f38a672a5abc8af74f9b84c8a3fcbabcb1
Ingrid Gama <ingridgamaa96@gmail.com>
2024-11-21 18:07:16 -0300

    result 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,16 @@
 # How many new outputs were created by block 123,456?
+
+BLOCK_NUMBER=123456
+
+BLOCK_HASH=$(bitcoin-cli getblockhash $BLOCK_NUMBER)
+
+TRANSACTIONS=$(bitcoin-cli getblock $BLOCK_HASH | jq -r '.tx[]')
+
+OUTPUT_COUNT=0
+
+for TX in $TRANSACTIONS; do
+  TX_OUTPUTS=$(bitcoin-cli getrawtransaction $TX true | jq '.vout | length')
+  OUTPUT_COUNT=$((OUTPUT_COUNT + TX_OUTPUTS))
+done
+
+echo "$OUTPUT_COUNT"
