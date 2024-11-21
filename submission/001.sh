605339f3ef0fcfd3aa968d1e5fdda3e94f8cfd4e
Ingrid Gama <ingridgamaa96@gmail.com>
2024-11-21 16:53:49 -0300

    script 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,1 +1,12 @@
 # What is the hash of block 654,321?
+
+# hash {000000000000000000058452bbe379ad4364fe8fda68c45e299979b492858095}
+
+
+BLOCK_NUMBER=654321
+
+BLOCK_HASH=$(bitcoin-cli getblockhash $BLOCK_NUMBER)
+
+echo "Hash do bloco $BLOCK_NUMBER: $BLOCK_HASH"
+
+
