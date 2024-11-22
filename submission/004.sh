297c8e0fdb11152012dee8facc6dfae9410a7745
Ingrid Gama <ingridgamaa96@gmail.com>
2024-11-22 14:38:41 -0300

    004 final result

--- submission/004.sh
+++ submission/004.sh
@@ -1,8 +1,10 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
+
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
-ADDRESS=$(bitcoin-cli deriveaddresses "tr($XPUB/0/0/100)#egpeepum" | tr -d '[]"')
-
+DESCRIPTOR="tr($XPUB/100)"
+DESCRIPTOR_WITH_CHECKSUM=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
+ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM" | jq -r '.[0]')
 echo $ADDRESS
