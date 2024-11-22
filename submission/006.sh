a647b6fd03377d9bedd361f7c7410c5abe2164e2
Ingrid Gama <ingridgamaa96@gmail.com>
2024-11-22 20:15:17 -0300

    Find signer of tx input 0 and trace coinbase spender tx

--- submission/006.sh
+++ submission/006.sh
@@ -1,2 +1,15 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
 
+block_origin=256128
+block_target=257343
+
+origin_block_hash=$(bitcoin-cli getblockhash $block_origin)
+
+coinbase_txid=$(bitcoin-cli getblock $origin_block_hash | jq -r '.tx[0]')
+target_block_hash=$(bitcoin-cli getblockhash $block_target)
+bitcoin-cli getblock $target_block_hash | jq -r '.tx[]' | while read txid; do
+    if bitcoin-cli getrawtransaction $txid true | jq -r '.vin[] | select(.txid == "'$coinbase_txid'") | .txid' | grep -q "$coinbase_txid"; then
+        echo "$txid"
+        exit 0
+    fi
+done
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,10 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+TXID="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+RAW_TX=$(bitcoin-cli getrawtransaction "$TXID" true)
+TXINWITNESS=$(echo "$RAW_TX" | jq -r '.vin[0].txinwitness[]')
+SCRIPT=$(echo "$TXINWITNESS" | tail -n 1)
+PUBKEY=$(echo "$SCRIPT" | grep -oE "02[0-9a-fA-F]{64}" | head -n 1)
+echo "$PUBKEY"
