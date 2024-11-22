c578464dcde4d56da50a3568f0c3f9023817a3c2
Ingrid Gama <ingridgamaa96@gmail.com>
2024-11-22 17:22:29 -0300

    resolution 005

--- submission/004.sh
+++ submission/004.sh
@@ -1,7 +1,6 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
-
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
 DESCRIPTOR="tr($XPUB/100)"
--- submission/005.sh
+++ submission/005.sh
@@ -1,3 +1,17 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
 
+pubkeys=(
+    "02bbb4ba3f39b5f3258f0014d5e4eab5a6990009e3e1dba6e8eaff10b3832394f7"
+    "03aaf17b1a7b4108f7e5bc4f7d59c20f7fb1a72dbc74a9a3d6d1f8488df159c760"
+    "03a6d919c76d9117c23570a767450013edf31cf6be7d3b5a881c06a9aa1f2c24ce"
+    "0383d12258e3e294a6d7754336f6b4baef992ec4b91694d3460bcb022b11da8cd2"
+)
+
+redeem_script="1"
+for pubkey in "${pubkeys[@]}"; do
+    redeem_script+=" $pubkey"
+done
+redeem_script+=" 4 OP_CHECKMULTISIG"
+p2sh_address=$(bitcoin-cli -named createmultisig nrequired=1 keys='["'${pubkeys[0]}'","'${pubkeys[1]}'","'${pubkeys[2]}'","'${pubkeys[3]}'"]' | jq -r .address)
+echo "$p2sh_address"
